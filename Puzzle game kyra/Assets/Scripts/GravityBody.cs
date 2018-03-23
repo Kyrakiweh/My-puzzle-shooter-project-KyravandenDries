705c6aced30efbe529cae3e9d4496f9c5597264b
Kyrakiweh <36536464+Kyrakiweh@users.noreply.github.com>
2018-03-23 15:24:12 +0100

    Les
    Animatie, dino, UI

---
+++ Puzzle game kyra/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour {
+    public GravityMoon attractor;
+    private Transform myTransform;
+    // Use this for initialization
+
+    void Start () {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        myTransform = transform;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        attractor.Attract(myTransform);
+	}
+}
---
+++ Puzzle game kyra/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour {
+    public float movespeed = 15;
+    private Vector3 movedir;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movedir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+	}
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + Transform.TransformDirection()
+    }
+}
